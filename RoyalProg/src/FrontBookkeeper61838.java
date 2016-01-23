960914dbfb5691d7858527a9d60981b268a45f8a
Desislava Asenova <dess.95@abv.bg>
2016-01-23 19:51:32 +0200

    Update FrontBookkeeper61838.java
    not sure if I'm doing this right..

--- RoyalProg/src/FrontBookkeeper61838.java
+++ RoyalProg/src/FrontBookkeeper61838.java
@@ -1,6 +1,84 @@
 import java.util.LinkedList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class FrontBookkeeper61838 implements IFrontBookkeeper {
+	private class Army {
+		private Map<String, LinkedList<String>> army; //key - unit name, value - soldiers in unit
+		private Map<String, LinkedList<String>> soldiers; //key - soldier id, value - units
+		private Map<String,String> attached; // if the unit (key) is attached to other unit already the value is the unit it is attached to, else it's "OK"
+	
+		public Army(){
+			army=new HashMap<>();
+			soldiers=new HashMap<>();
+			attached=new HashMap<>();
+		}
+		
+		public void addUnit(String unitName,LinkedList<String> unit){
+			if(!army.containsKey(unitName)){
+				army.put(unitName,unit);
+				attached.put(unitName, "ok");
+			}else{
+				army.get(unitName).addAll(unit);
+			}
+			for(String soldier:unit){
+				if(!soldiers.containsKey(soldier)){
+					LinkedList<String> units = new LinkedList<>();
+					units.add(unitName);
+					soldiers.put(soldier, units);
+				}
+				else{
+					soldiers.get(soldier).add(unitName);
+				}
+			}
+		}
+		
+		public String showSoldier(String soldier){
+			String unitsPartOf = soldiers.get(soldier).toString();
+			return unitsPartOf.substring(1,unitsPartOf.length()-1);
+		}
+		
+		// fckn UGLY
+		public String showUnit(String unit){
+			//return army.get(unit).toString(); it's working but....
+			String result="";
+			if(army.get(unit).size()>1){
+				for(String soldiers:army.get(unit)){
+					if(!soldiers.equals("")){
+						result+=soldiers+", ";
+					}
+				}
+				result=result.substring(0,result.length()-2);
+			}
+			return "["+result+"]";
+		}
+		
+		public void attachUnit(String unit1, String unit2){
+			if(attached.get(unit1).equals("ok")){
+				addUnit(unit2,army.get(unit1));
+				attached.put(unit1, unit2);
+			}else{
+				String alreadyAttachedTo=attached.get(unit1);
+				army.get(alreadyAttachedTo).removeAll(army.get(unit1));
+				addUnit(unit2,army.get(unit1));
+			}
+		}
+		
+		public void killSoldiers(int x, int y,String unitName){
+			for(int i=x;i<=y;i++){
+				String soldier = new Integer(i).toString();
+				if(soldiers.containsKey(soldier)){
+					soldiers.remove(soldier);
+					for(String unit:army.keySet()){
+						army.get(unit).remove(soldier);
+					}
+				}else{
+					continue;
+				}
+			}
+		}
+	}
+	
 	String result="";
 	@Override
 	public String updateFront(String[] news) {
